c98d99111481c3296453ac1ae9f3dab7c069a4ac
Fellipe Rezende <rezendefellipe9@gmail.com>
2021-06-01 16:37:17 -0300

    :tada: Initial commit, created header

---
+++ src/pages/_document.tsx
@@ -0,0 +1,25 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Alfa+Slab+One&family=Gentium+Book+Basic&display=swap"
+            rel="stylesheet"
+          />
+
+          <link rel="shortcut icon" href="/favicon.png" type="image/png" />
+
+          <title>fr.news</title>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
