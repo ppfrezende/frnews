fa0a46881f99839559cd416d700b05c863fbf124
Fellipe Rezende <rezendefellipe9@gmail.com>
2021-06-02 09:26:47 -0300

    :sparkles: authentication using next auth with github

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,19 @@
+import { GetStaticProps } from 'next';
+
 import Head from 'next/head';
 import { SubscribeButton } from '../components/SubscribeButton';
+import { stripe } from '../services/stripe';
 
 import styles from './home.module.scss';
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  };
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
@@ -19,10 +29,10 @@ export default function Home() {
 
           <p>
             Tenha acesso a todo conteúdo dos posts <br />
-            <span>por apenas R$7,99</span>
+            <span>por apenas {product.amount} por mês</span>
           </p>
 
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId} />
         </section>
 
         <img src="/images/avatar.svg" alt="Girl Power" />
@@ -30,3 +40,22 @@ export default function Home() {
     </>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve('price_1IxelaCyPluncVJQmeYklBa8');
+
+  const product = {
+    priceId: price.id,
+    amount: Intl.NumberFormat('pt-BR', {
+      style: 'currency',
+      currency: 'BRL',
+    }).format(price.unit_amount / 100),
+  };
+
+  return {
+    props: {
+      product,
+    },
+    revalidate: 60 * 60 * 24, // 24 hours
+  };
+};
